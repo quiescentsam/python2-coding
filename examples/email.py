c6c955b04b0e74995dede786a5b2e9cd991f7787
sam <sameerahmad@cloudera.com>
2019-02-19 14:09:06 -0500

    2019021914

--- examples/email.py
+++ examples/email.py
@@ -1,6 +1,7 @@
 import smtplib, ssl
 
-port = 587  # For starttls
+#port = 587  # For starttls
+port = 465  # For SSL
 smtp_server = "smtp.gmail.com"
 sender_email = "sameerautomate@gmail.com"
 receiver_email = "chill.sameer@gmail.com"
@@ -11,10 +12,15 @@ Subject: Hi there
 
 This message is sent from Python."""
 
+# context = ssl.create_default_context()
+# with smtplib.SMTP(smtp_server, port) as server:
+#     server.ehlo()  # Can be omitted
+#     server.starttls(context=context)
+#     server.ehlo()  # Can be omitted
+#     server.login(sender_email, password)
+#     server.sendmail(sender_email, receiver_email, message)
+
 context = ssl.create_default_context()
-with smtplib.SMTP(smtp_server, port) as server:
-    server.ehlo()  # Can be omitted
-    server.starttls(context=context)
-    server.ehlo()  # Can be omitted
+with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
     server.login(sender_email, password)
     server.sendmail(sender_email, receiver_email, message)
