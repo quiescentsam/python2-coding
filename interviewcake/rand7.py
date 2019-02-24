c3b850746fadabc7a71948fc5eaae020f98b0899
sam <sameerahmad@cloudera.com>
2019-02-23 22:21:09 -0500

    2019022322

--- interviewcake/rand7.py
+++ interviewcake/rand7.py
@@ -3,6 +3,12 @@ import random
 def rand5():
     return random.randint(0,5)
 
+
+def rand7():
+    return ((rand5() + rand5() + rand5() + rand5() + rand5() + rand5() + rand5()) % 7 )
+
+
 if __name__ == '__main__':
     print rand5()
+    print rand7()
 
